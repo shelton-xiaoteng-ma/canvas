dbc46d9b8c6b705e58a6dd292451d29ec60f726a
maxiaoteng <jizhuwo00@hotmail.com>
2024-11-25 16:00:43 +0800

    Oauth: Google

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,12 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 import { db } from "./db/drizzle";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
+  providers: [GitHub, Google],
   adapter: DrizzleAdapter(db),
   pages: {
     signIn: "/sign-in",
